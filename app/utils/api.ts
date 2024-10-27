057b98a9f9c9076f107b38dbbb0ace85c8a29933
zayprom <zayakov@promoplace.cz>
2024-10-27 06:41:24 +0100

    update note api

--- app/utils/api.ts
+++ app/utils/api.ts
@@ -1,7 +1,19 @@
 import { db } from "./db.server";
+import { Note } from "./types";
 
 export const getNoteById = async (noteId: number) => {
   return await db.note.findUnique({
     where: { id: noteId },
+  });
+};
+
+export const updateNote = async (noteId: number, data: Note) => {
+  const note = await getNoteById(noteId);
+  if (!note) {
+    throw new Error(`No contanct found for ${note}`);
+  }
+  return await db.note.update({
+    where: { id: note.id },
+    data: data,
   });
 };
