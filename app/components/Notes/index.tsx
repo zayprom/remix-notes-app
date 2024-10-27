92a86b185108bb4827cd4de48fbe98c4c162565d
zayprom <zayakov@promoplace.cz>
2024-10-27 20:49:46 +0100

    notes prototyping

--- app/components/Notes/index.tsx
+++ app/components/Notes/index.tsx
@@ -1,5 +1,6 @@
 import { NavLink } from "@remix-run/react";
 import { Note } from "~/utils/types";
+import styles from "./styles.module.css";
 
 interface NotesListProps {
   notes: Note[];
@@ -7,7 +8,7 @@ interface NotesListProps {
 
 export const NotesList = (props: NotesListProps) => {
   return (
-    <ul className="notesList">
+    <ul className={styles.notesList}>
       {props.notes.map((note) => (
         <li key={note.id}>
           <NavLink to={`notes/${note.id}`}>{note.title}</NavLink>
