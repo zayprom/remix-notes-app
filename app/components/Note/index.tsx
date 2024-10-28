b15d0f1df9c983e5421bc4817316d2422c75b16a
zayprom <zayakov@promoplace.cz>
2024-10-28 05:50:57 +0100

    SingleNote added

---
+++ app/components/Note/index.tsx
@@ -0,0 +1,18 @@
+import { Note } from "~/utils/types";
+import { NavLink } from "@remix-run/react";
+import styles from "./styles.module.css";
+
+interface SingleNoteProps {
+  note: Note;
+}
+
+export const SingleNote = (props: SingleNoteProps) => {
+  return (
+    <>
+      <li className={styles.li} key={props.note.id}>
+        <NavLink to={`notes/${props.note.id}`}>{props.note.title}</NavLink>
+        <NavLink to={`notes/${props.note.id}/edit`}>Edit</NavLink>
+      </li>
+    </>
+  );
+};
