ac3de1f1beb2abf23fcdf3bce37a3ee5ca895c50
zayprom <zayakov@promoplace.cz>
2024-11-09 06:37:31 +0100

    sidebar improved

--- app/components/Sidebar/index.tsx
+++ app/components/Sidebar/index.tsx
@@ -1,5 +1,4 @@
 import { ReactNode } from "react";
-import { Outlet } from "@remix-run/react";
 import styles from "./styles.module.css";
 
 interface SideBarProps {
@@ -7,10 +6,5 @@ interface SideBarProps {
 }
 
 export const SideBar = (props: SideBarProps) => {
-  return (
-    <div className={styles.sidebar}>
-      {props.children}
-      <Outlet />
-    </div>
-  );
+  return <div className={styles.sidebar}>{props.children}</div>;
 };
