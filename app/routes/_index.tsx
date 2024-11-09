13f6123b7950aee991ed01ae442e057832506cb2
zayprom <zayakov@promoplace.cz>
2024-11-09 06:37:18 +0100

    adjust landing index

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,15 +1,5 @@
-import type {
-  ActionFunctionArgs,
-  LinksFunction,
-  LoaderFunctionArgs,
-  MetaFunction,
-} from "@remix-run/node";
-import { json, redirect, useLoaderData } from "@remix-run/react";
-import { SideBar } from "~/components/Sidebar";
-import { db } from "~/utils/db.server";
+import type { MetaFunction, LinksFunction } from "@remix-run/node";
 import "../app.css";
-import { NotesList } from "~/components/Notes";
-import { NewNote } from "~/components/Details/New";
 import stylesHref from "../app.css?url";
 
 export const meta: MetaFunction = () => {
@@ -33,44 +23,10 @@ export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesHref },
 ];
 
-export async function loader({ request }: LoaderFunctionArgs) {
-  const notes = await db.note.findMany({
-    orderBy: { createdAt: "desc" },
-  });
-
-  return json({ notes });
-}
-
-export async function action({ request }: ActionFunctionArgs) {
-  const formData = await request.formData();
-  const title = formData.get("title");
-  const content = formData.get("content");
-
-  if (typeof title !== "string" || typeof content !== "string") {
-    return { formError: "Form not submitted correctly." };
-  }
-
-  const newNote = await db.note.create({
-    data: {
-      title,
-      content,
-    },
-  });
-
-  return redirect(`notes/${newNote.id}`);
-}
-
 export default function Index() {
-  const { notes } = useLoaderData<typeof loader>();
   return (
-    <main>
-      <SideBar>
-        <header>
-          <h1>Welcome to Remix-notes-app</h1>
-        </header>
-        <NotesList notes={notes} />
-      </SideBar>
-      <NewNote />
-    </main>
+    <header>
+      <h1>Welcome to Remix-notes-app</h1>
+    </header>
   );
 }
