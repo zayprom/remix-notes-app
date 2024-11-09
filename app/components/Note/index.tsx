7d9ea526281248b8d894ff429a1e769152e10549
zayprom <zayakov@promoplace.cz>
2024-11-09 07:47:27 +0100

    initial styling changes

--- app/components/Note/index.tsx
+++ app/components/Note/index.tsx
@@ -9,8 +9,15 @@ interface SingleNoteProps {
 export const SingleNote = (props: SingleNoteProps) => {
   return (
     <li className={styles.li}>
-      <NavLink to={`notes/${props.note.id}`}>{props.note.title}</NavLink>
-      <NavLink to={`notes/${props.note.id}/edit`}>Edit</NavLink>
+      <div className={styles.noteHeading}>
+        <NavLink to={`notes/${props.note.id}`} className={styles.titleLink}>
+          {props.note.title}
+        </NavLink>
+        <NavLink to={`notes/${props.note.id}/edit`}>Edit</NavLink>
+      </div>
+      <div className={styles.info}>
+        <span>{props.note.content}</span>
+      </div>
     </li>
   );
 };
